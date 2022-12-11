6785dea96864982d5b0a1a47ae502b65f9890ab5
Madden <AL07839@LDD70HMQQXX.US.AD.WELLPOINT.COM>
2022-12-11 19:45:39 +0000

    Zap report header updates

--- BPCalculator/Program.cs
+++ BPCalculator/Program.cs
@@ -14,6 +14,7 @@ namespace BPCalculator
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            app.UseHttpsRedirection();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- BPCalculator/Startup.cs
+++ BPCalculator/Startup.cs
@@ -43,17 +43,12 @@ namespace BPCalculator
 
             app.UseAuthorization();
 
-            //This implements content security policy
-            app.Use(async (context, next) =>
-            {
-                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self';");
-                await next();
-            });
-
             // This method adds X-FRAME-OPTIONS for every request
             app.Use(async (context, next) =>
             {
                 context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+                context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self';");
                 await next();
             });
 
